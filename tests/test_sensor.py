999e9fad7bd44569baa4eab34628f860073fbcfa
Aaron Godfrey <me@aarongodfrey.dev>
2020-10-05 19:52:27 -0700

    Adding unittests for find_matching_config_entry_for_host.

---
+++ tests/test_sensor.py
@@ -0,0 +1,45 @@
+"""Tests for sensor.py."""
+from unittest.mock import Mock
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.kodi_recently_added.sensor import (
+    KODI_DOMAIN,
+    find_matching_config_entry_for_host,
+)
+
+
+def test_find_matching_config_entry_for_host():
+    """Test we find a matching config entry."""
+    config_entries = [MockConfigEntry(domain=KODI_DOMAIN, data={"host": "127.0.0.1"})]
+    mock_hass = Mock()
+    mock_hass.config_entries.async_entries.return_value = config_entries
+
+    assert config_entries[0] == find_matching_config_entry_for_host(
+        mock_hass, "127.0.0.1"
+    )
+
+
+def test_find_matching_config_entry_for_host_does_not_exist():
+    """Test we find no matching config entry."""
+    config_entries = [MockConfigEntry(domain=KODI_DOMAIN, data={"host": "127.0.0.1"})]
+    mock_hass = Mock()
+    mock_hass.config_entries.async_entries.return_value = config_entries
+
+    assert find_matching_config_entry_for_host(mock_hass, "192.168.1.1") is None
+
+
+def test_find_matching_config_entry_for_host_skip_source_ignore():
+    """Test we skip matches whose source is ignore."""
+    config_entries = [
+        MockConfigEntry(
+            domain=KODI_DOMAIN, data={"host": "127.0.0.1"}, source="ignore"
+        ),
+        MockConfigEntry(domain=KODI_DOMAIN, data={"host": "127.0.0.1"}, source="user"),
+    ]
+    mock_hass = Mock()
+    mock_hass.config_entries.async_entries.return_value = config_entries
+
+    assert config_entries[1] == find_matching_config_entry_for_host(
+        mock_hass, "127.0.0.1"
+    )
