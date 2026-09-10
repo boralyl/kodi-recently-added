a6ab61a1dfc865ab354c8b8de4a57761deab8b03
Aaron Godfrey <me@aarongodfrey.dev>
2026-09-09 20:41:34 -0700

    Fix OptionsFlowHandler using legacy method
    The config_entry is now inherited by the base class, and not longer
    needs to be set.

--- custom_components/kodi_recently_added/config_flow.py
+++ custom_components/kodi_recently_added/config_flow.py
@@ -55,14 +55,11 @@ class KodiRecentlyAddedConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry):
         """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handles options flow for the component."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
